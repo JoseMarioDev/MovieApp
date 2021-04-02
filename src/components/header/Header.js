c59b4f282620f15ec81f0ae5dd2eb73b5a2fe3a9
JoseMarioDev <josemariodev@gmail.com>
2021-04-02 09:18:02 -0500

    :sparkles: Creating API for movie detail and header style fixes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 
 import './Header.scss';
 import logo from '../../img/Logo.png';
@@ -28,10 +29,16 @@ const Header = (props) => {
   const [type, setType] = useState('now_playing');
   const [search, setSearch] = useState('');
 
+  const history = useHistory();
+
   useEffect(() => {
     getMovies(type, page);
     setResponsePageNumber(page, totalPages);
   }, [type]);
+
+  const navigateToMainPage = () => {
+    history.push('/');
+  };
 
   const setMovieTypeUrl = (type) => {
     setType(type);
@@ -61,7 +68,7 @@ const Header = (props) => {
       <div className="header-nav-wrapper">
         <div className="header-bar"></div>
         <div className="header-navbar">
-          <div className="header-image">
+          <div className="header-image" onClick={() => navigateToMainPage()}>
             <img src={logo} alt="logo" />
           </div>
           <div
