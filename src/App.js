b64c1463801e6598c4ac51138755c3cd8757bc7b
JoseMarioDev <josemariodev@gmail.com>
2021-04-02 09:16:29 -0500

    :art: Adding route for movie-details

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.scss';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import store from './redux/store';
+import MovieDetails from './components/content/movie-details/MovieDetails';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Header />
-      <div className="app">
-        <Main />
-      </div>
+      <Router>
+        <Header />
+        <div className="app">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/:id/:name/details" component={MovieDetails} />
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 };
